b6ff199cac81a6fe436c4398ca629d73cb979e53
asd <m.mishchenko@abtollc.com>
2024-07-24 20:25:19 +0300

    added Accept type

--- src/file-props.ts
+++ src/file-props.ts
@@ -1,12 +1,13 @@
 import { Data } from "leva/src/types";
+import { Accept } from "react-dropzone";
 
 export const normalize = (
-  input: { onChange: (f: any) => void, accept?: string },
-  path: string,
-  data: Data
+  input: { onChange: (f: any) => void, accept?: Accept },
+  _path: string,
+  _data: Data
 ) => {
   return {
     value: input,
-    settings: { accept: input?.accept }
+    settings: { accept: input.accept }
   };
 };
